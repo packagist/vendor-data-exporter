0cea5bd47dddc5c9c23f72033fe72a131ef0fa89
Zan Baldwin <hello@zanbaldwin.com>
2022-08-11 19:14:05 +0200

    $ composer req --dev phpstan/phpstan phpstan/phpstan-strict-rules phpstan/phpstan-deprecation-rules friendsofphp/php-cs-fixer
    Add development tools; code style and static analysis.

---
+++ .php-cs-fixer.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/bin')
+;
+
+return (new PhpCsFixer\Config)
+    ->setUsingCache(true)
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setRules([
+        'void_return' => true,
+        'no_empty_statement' => true,
+        'no_unused_imports' => true,
+        'declare_strict_types' => true,
+        'no_extra_blank_lines' => true,
+        'no_whitespace_in_blank_line' => true,
+        'no_blank_lines_after_class_opening' => true,
+        'semicolon_after_instruction' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters']],
+        'no_superfluous_phpdoc_tags' => ['allow_mixed' => true],
+        'no_empty_phpdoc' => true,
+        'phpdoc_trim' => true,
+        'phpdoc_types' => true,
+        'ternary_operator_spaces' => true,
+        'binary_operator_spaces' => true,
+        'trim_array_spaces' => true,
+        'concat_space' => ['spacing' => 'one'],
+    ]);
