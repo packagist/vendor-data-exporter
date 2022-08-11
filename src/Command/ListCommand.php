603e8194ef51cf838c6a4eaae3cc3360f1f4ebb6
Zan Baldwin <hello@zanbaldwin.com>
2022-08-11 19:19:03 +0200

    $ composer req private-packagist/api-client guzzlehttp/guzzle php-http/guzzle7-adapter
    Set up the Private Packagist SDK inside the console command.

--- src/Command/ListCommand.php
+++ src/Command/ListCommand.php
@@ -2,7 +2,9 @@
 
 namespace PrivatePackagist\VendorDataExporter\Command;
 
+use PrivatePackagist\ApiClient\Client as PackagistSdk;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -19,11 +21,30 @@ class ListCommand extends Command
     {
         $this
             ->setName(self::DEFAULT_COMMAND_NAME)
+            ->addOption('token', null, InputOption::VALUE_REQUIRED, 'Private Packagist API Token')
+            ->addOption('secret', null, InputOption::VALUE_REQUIRED, 'Private Packagist API Secret')
             ->setDescription('List package versions that a vendor\'s customers have access to.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $client = $this->getPackagistApiClient($input);
+
         return 0;
+    }
+
+    private function getPackagistApiClient(InputInterface $input): PackagistSdk
+    {
+        if (!is_string($token = $input->getOption('token'))) {
+            $token = $_ENV['PACKAGIST_API_TOKEN'] ?? throw new \InvalidArgumentException('Missing API credentials: provide API token via command flag or environment variable.');
+        }
+        if (!is_string($secret = $input->getOption('secret'))) {
+            $secret = $_ENV['PACKAGIST_API_SECRET'] ?? throw new \InvalidArgumentException('Missing API credentials: provide API secret via command flag or environment variable.');
+        }
+
+        $apiClient = new PackagistSdk(null, $_ENV['PACKAGIST_API_URL'] ?? null);
+        $apiClient->authenticate($token, $secret);
+
+        return $apiClient;
     }
 }
