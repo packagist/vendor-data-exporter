ded5b5a33b2d536f75b35c91180131e2b8cbe323
Zan Baldwin <hello@zanbaldwin.com>
2022-08-22 17:47:25 +0200

    Use HTTPlug's Mock Client

--- tests/Command/ListCommandTest.php
+++ tests/Command/ListCommandTest.php
@@ -2,30 +2,51 @@
 
 namespace PrivatePackagist\VendorDataExporter\Test\Command;
 
+use GuzzleHttp\Psr7\Response;
+use Http\Message\RequestMatcher;
+use Http\Mock\Client as MockHttpClient;
 use PHPUnit\Framework\TestCase;
 use PrivatePackagist\ApiClient\Client as PackagistApiClient;
 use PrivatePackagist\ApiClient\HttpClient\HttpPluginClientBuilder;
 use PrivatePackagist\VendorDataExporter\Command\ListCommand;
-use PrivatePackagist\VendorDataExporter\Test\MockHttpClient;
-use PrivatePackagist\VendorDataExporter\Test\RequestCounterInterface;
-use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 class ListCommandTest extends TestCase
 {
     protected readonly PackagistApiClient $packagistApiClient;
-    protected readonly ClientInterface&RequestCounterInterface $httpClient;
+    protected readonly MockHttpClient $httpClient;
     private Command $command;
 
     public function setUp(): void
     {
-        $this->httpClient = new MockHttpClient;
+        $this->httpClient = $this->createMockHttpClientUsingSavedApiResponsesFromDirectory(__DIR__ . '/../res/http');
         $this->packagistApiClient = new PackagistApiClient(new HttpPluginClientBuilder($this->httpClient));
 
         $this->command = new ListCommand;
         $this->command->setPackagistApiClient($this->packagistApiClient);
+    }
+
+    private function createMockHttpClientUsingSavedApiResponsesFromDirectory(string $responsesDirectory): MockHttpClient
+    {
+        $httpClient = new MockHttpClient;
+        $httpClient->on(new class implements RequestMatcher {
+            public function matches(RequestInterface $request): bool {
+                return strtoupper($request->getMethod()) === 'GET';
+            }
+        }, function (RequestInterface $request) use ($responsesDirectory): ResponseInterface {
+            $requestIdentifier = sprintf('%s %s', $request->getMethod(), $request->getUri()->getPath());
+            $httpResponseFilepath = sprintf('%s/%s.json', $responsesDirectory, (new AsciiSlugger)->slug($requestIdentifier)->lower()->toString());
+            if (!file_exists($httpResponseFilepath) || !is_readable($httpResponseFilepath) || !is_string($mockHttpResponse = file_get_contents($httpResponseFilepath))) {
+                static::fail(sprintf('Mock response not found for "%s"; tried looking for file "%s".', $requestIdentifier, $httpResponseFilepath));
+            }
+            return new Response(200, ['Content-Type' => 'application/json'], $mockHttpResponse);
+        });
+        return $httpClient;
     }
 
     public function testTextOutput(): void
@@ -35,7 +56,7 @@ class ListCommandTest extends TestCase
 
         $this->command->run($input, $output);
 
-        static::assertSame(49, $this->httpClient->getRequestCount());
+        static::assertCount(49, $this->httpClient->getRequests());
         static::assertSame(file_get_contents(__DIR__ . '/../res/output/output.txt'), $output->fetch());
     }
 
@@ -46,7 +67,7 @@ class ListCommandTest extends TestCase
 
         $this->command->run($input, $output);
 
-        static::assertSame(49, $this->httpClient->getRequestCount());
+        static::assertCount(49, $this->httpClient->getRequests());
         static::assertSame(file_get_contents(__DIR__ . '/../res/output/output.json'), $output->fetch());
     }
 
@@ -57,7 +78,7 @@ class ListCommandTest extends TestCase
 
         $this->command->run($input, $output);
 
-        static::assertSame(49, $this->httpClient->getRequestCount());
+        static::assertCount(49, $this->httpClient->getRequests());
         static::assertSame(file_get_contents(__DIR__ . '/../res/output/output.csv'), $output->fetch());
     }
 }
--- tests/RequestCounterInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace PrivatePackagist\VendorDataExporter\Test;
-
-interface RequestCounterInterface
-{
-    public function getRequestCount(): int;
-}
